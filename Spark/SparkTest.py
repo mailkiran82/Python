a98160278735e28aab9f2dc18408d7dc25f33857
mailkiran82 <mailkiran82@gmail.com>
2018-08-02 21:40:07 +0530

    add smart_open

--- Spark/SparkTest.py
+++ Spark/SparkTest.py
@@ -5,20 +5,60 @@ import pyspark
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import countDistinct,avg,stddev
+from pyspark.sql.types import StructType,StructField,IntegerType,StringType
 
 import boto3
 import timeit
+import pandas as pd
+import io
+import smart_open as so
+
+# get the file into pandas dataframe as object
+header=True
+headerList=None
+pdf=pd.DataFrame()
+with so.smart_open('s3://mailkiran82/SparkData/TestData.csv') as fin:
+    for line in fin:
+        if(header):
+            headerList=list(line.decode('utf-8').lstrip().rstrip().split(','))
+            header=False
+        else:
+            rec=pd.DataFrame([list(line.decode('utf-8').lstrip().rstrip().split(','))],columns=headerList)
+            pdf=pd.concat([pdf,rec],ignore_index=True)
 
 print(timeit.timeit('''
-import boto3
-s3 = boto3.resource('s3')
-s3.Object('mailkiran82','SparkData/TestData.csv').download_file('./TD_Download.csv')
+import pandas as pd
+import smart_open as so
+header=True
+headerList=None
+pdf=pd.DataFrame()
+with so.smart_open('s3://mailkiran82/SparkData/TestData.csv') as fin:
+    for line in fin:
+        if(header):
+            headerList=list(line.decode('utf-8').lstrip().rstrip().split(','))
+            header=False
+        else:
+            rec=pd.DataFrame([list(line.decode('utf-8').lstrip().rstrip().split(','))],columns=headerList)
+            pdf=pd.concat([pdf,rec],ignore_index=True)
 ''',number=1))
 
-s3 = boto3.resource('s3')
-s3.Object('mailkiran82','SparkData/TestData.csv').download_file('./TD.csv')
+#pdf['id']=pdf['id'].astype(int)
+#print(pdf.head())
+#print(pdf.info())
+
+#s3 = boto3.client('s3')
+#res = s3.get_object(Bucket='mailkiran82',Key='SparkData/TestData.csv')
+#text = res['Body'].read().decode('utf-8')
+#pdf1 = pd.read_csv(io.StringIO(text))
+#print(pdf1.head())
+#print(pdf1.info())
+
+#populate Spark dataframe from pandas dataframe
 spark = SparkSession.builder.appName('ops').getOrCreate()
-df = spark.read.csv('TD.csv',inferSchema=True, header=True)
+#schema = StructType([StructField('id',StringType(),True)\
+#                     ,StructField('name',StringType(),True)])
+#df = spark.createDataFrame(pdf,schema=schema)
+df = spark.createDataFrame(pdf)
 
 
 def getCount():
@@ -31,7 +71,7 @@ def getCount():
 def main():
     res = getCount()
     print(res)
-    print("Done")
+    print('Done')
 
 if __name__ == '__main__':
     main()
