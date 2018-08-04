806e2c655b4bd857bae87f796339f3eba444acf2
mailkiran82 <mailkiran82@gmail.com>
2018-08-04 17:28:18 +0530

    test

--- Spark/TestSparkTest.py
+++ Spark/TestSparkTest.py
@@ -6,7 +6,7 @@ import SparkTest
 class TestSparkTest1(unittest.TestCase):
     def test_getCount(self):
         res = SparkTest.getCount()
-        self.assertEqual(res,6)
+        self.assertEqual(res,7)
 
 
 if __name__ == '__main__':
